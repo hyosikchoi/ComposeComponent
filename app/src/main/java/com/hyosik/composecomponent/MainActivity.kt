258b6a08c25858bec5d30a164ce50bac079a1b11
hyosikchoi <huchoi24@naver.com>
2024-02-01 01:05:59 +0900

    Add - Text

--- app/src/main/java/com/hyosik/composecomponent/MainActivity.kt
+++ app/src/main/java/com/hyosik/composecomponent/MainActivity.kt
@@ -4,12 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.hyosik.composecomponent.ui.theme.ComposeComponentTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,7 +39,26 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+
+    // 1. color 지정
+//    Text(color = Color.Red , text = "Hello $name")
+
+    // 2. color 객체로 해쉬값으로 지정 (ARGB)
+//    Text(color = Color(0xffff9944) , text = "Hello $name")
+
+    // fontsize, fontweight, fontfamily , letterSpacing(글자간 간격)
+    // textDecoration (밑줄과 같은 옵션을 설정할 수 있다.)
+    Text(
+        modifier = Modifier.width(300.dp),
+        text = "Hello $name\nHello $name\nHello $name\n",
+        fontSize = 30.sp,
+        fontWeight = FontWeight.Bold,
+        fontFamily = FontFamily.Cursive,
+        letterSpacing = 2.sp,
+        maxLines = 2,
+        textDecoration = TextDecoration.Underline,
+        textAlign = TextAlign.Center
+    )
 }
 
 @Preview(showBackground = true)
