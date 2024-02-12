7c3717f1b3bfad787f58f5fdb6fc48a522fcffb7
hyosikchoi <huchoi24@naver.com>
2024-02-12 21:45:02 +0900

    Add - Todo

--- app/src/main/java/com/hyosik/composecomponent/MainActivity.kt
+++ app/src/main/java/com/hyosik/composecomponent/MainActivity.kt
@@ -1,20 +1,22 @@
 package com.hyosik.composecomponent
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.CheckBox
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -41,13 +43,211 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    HSSnackbar()
+                    TopLevel()
                 }
 
             }
         }
     }
 }
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun TopLevel() {
+    val (text, setText) = remember { mutableStateOf("") }
+    val toDoList = remember { mutableStateListOf<ToDoData>() }
+
+    val onSubmit: (String) -> Unit = { text ->
+        val key = (toDoList.lastOrNull()?.key ?: 0) + 1
+        toDoList.add(ToDoData(key = key, text = text))
+        setText("")
+    }
+
+    val onToggle: (Int, Boolean) -> Unit = { key, checked ->
+       val i = toDoList.indexOfFirst { it.key == key }
+        toDoList[i] = toDoList[i].copy(done = checked)
+    }
+
+    val onDelete: (Int) -> Unit = { key ->
+        val i = toDoList.indexOfFirst { it.key == key }
+        toDoList.removeAt(i)
+    }
+
+    val onEdit: (Int, String) -> Unit = { key, text ->
+        val i = toDoList.indexOfFirst { it.key == key }
+        toDoList[i] = toDoList[i].copy(key = key, text = text)
+    }
+    // 단계 4: `onSubmit`, `onEdit`, `onToggle`, `onDelete`를
+    // 만들어 `ToDo`에 연결합니다.
+
+    Scaffold {
+        Column {
+            ToDoInput(
+                text = text,
+                onTextChange = setText,
+                onSubmit = onSubmit
+            )
+            // 단계 3: `LazyColumn`으로 `toDoList`를 표시합시다.
+            /** `key`를 `toDoData`의 `key`를 사용합니다. */
+            /** diffUtil 에서의 areItemSame 과 비슷하다고 볼 수 있다. */
+            LazyColumn {
+                items(toDoList, key = {it.key}) { todoData: ToDoData ->
+                    ToDo(
+                        toDoData = todoData,
+                        onToggle = onToggle,
+                        onDelete = onDelete,
+                        onEdit = onEdit
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeComponentTheme {
+        TopLevel()
+    }
+}
+
+@Composable
+fun ToDoInput(
+    text: String,
+    onTextChange: (String) -> Unit,
+    onSubmit: (String) -> Unit
+) {
+    Row(modifier = Modifier.padding(8.dp)) {
+        OutlinedTextField(
+            value = text,
+            onValueChange = onTextChange,
+            modifier = Modifier.weight(1f)
+        )
+        Spacer(modifier = Modifier.size(8.dp))
+        Button(onClick = {
+            onSubmit(text)
+        }) {
+            Text("입력")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ToDoInputPreview() {
+    ComposeComponentTheme {
+        ToDoInput("테스트", {}, {})
+    }
+}
+
+@Composable
+fun ToDo(
+    toDoData: ToDoData,
+    onEdit: (key: Int, text: String) -> Unit = { _, _ -> },
+    onToggle: (key: Int, checked: Boolean) -> Unit = { _, _ -> },
+    onDelete: (key: Int) -> Unit = {}
+) {
+    var isEditing by remember { mutableStateOf(false) }
+    Card(
+        modifier = Modifier.padding(4.dp),
+        elevation = 8.dp
+    ) {
+
+        // 단계 1: `Row`를 만들고 `toDoData.text`를 출력하고
+        // 완료를 체크하는 체크박스, 수정 버튼, 삭제 버튼을 만드세요.
+//        Row(
+//            verticalAlignment = Alignment.CenterVertically
+//        ) {
+//            Text(
+//                modifier = Modifier.weight(1f),
+//                text = toDoData.text
+//            )
+//            Text(
+//                text = "완료"
+//            )
+//            Checkbox(checked = toDoData.done, onCheckedChange = { checked -> onToggle(toDoData.key, checked)})
+//            Button(onClick = {}) {
+//                Text(text = "수정")
+//            }
+//            Spacer(modifier = Modifier.size(4.dp))
+//            Button(onClick = {}) {
+//                Text(text = "삭제")
+//            }
+//        }
+
+        // 단계 2: `Crossfade`를 통해 `isEditing`을 따라 다른
+        // UI를 보여줍니다. `OutlinedTextField`와 `Button을
+        // 넣어봅시다.
+        Crossfade(targetState = isEditing) {
+            when(it) {
+                false -> {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.padding(8.dp)
+                    ) {
+                        Text(
+                            modifier = Modifier.weight(1f),
+                            text = toDoData.text
+                        )
+                        Text(
+                            text = "완료"
+                        )
+                        Checkbox(checked = toDoData.done, onCheckedChange = { checked -> onToggle(toDoData.key, checked)})
+                        Button(onClick = {
+                            isEditing = true
+                        }) {
+                            Text(text = "수정")
+                        }
+                        Spacer(modifier = Modifier.size(4.dp))
+                        Button(onClick = {
+                            onDelete(toDoData.key)
+                        }) {
+                            Text(text = "삭제")
+                        }
+                    }
+                }
+
+                true -> {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.padding(8.dp)
+                    ) {
+                        val (newText, setNewText) = remember { mutableStateOf(toDoData.text) }
+
+                        OutlinedTextField(
+                            value = newText,
+                            onValueChange = setNewText,
+                            modifier = Modifier.weight(1f)
+                        )
+                        Spacer(modifier = Modifier.size(4.dp))
+                        Button(
+                            onClick = {
+                                onEdit(toDoData.key, newText)
+                                isEditing = false
+                        }) {
+                            Text(text = "완료")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ToDoPreview() {
+    ComposeComponentTheme {
+        ToDo(ToDoData(1, "nice", true))
+    }
+}
+
+data class ToDoData(
+    val key: Int,
+    val text: String,
+    val done: Boolean = false
+)
 
 
 @Composable
@@ -89,28 +289,6 @@ fun CatalogEx(itemList: List<ItemData>) {
     }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun ItemPreview() {
-    ComposeComponentTheme {
-        Item(
-            itemData = ItemData(
-                imageId = R.drawable.broly,
-                title = "브로리",
-                description = "드래곤볼 슈퍼 브로리 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-            )
-        )
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeComponentTheme {
-        CatalogEx(items)
-    }
-}
-
 
 val items = listOf<ItemData>(
     ItemData(
@@ -136,50 +314,4 @@ val items = listOf<ItemData>(
 )
 
 
-@Preview(showBackground = true)
-@Composable
-fun ConstraintPreview() {
-    ComposeComponentTheme {
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colors.background
-        ) {
-            HSConstraintLayout()
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun ConstraintChainPreview() {
-    ComposeComponentTheme {
-        Surface(
-            modifier = Modifier.fillMaxSize()
-        ) {
-            HSConstraintLayoutChain()
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun CardExPreview() {
-    ComposeComponentTheme() {
-        Surface(
-        ) {
-            HSCard()
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun CanvasPreview() {
-    ComposeComponentTheme() {
-        Surface(
-        ) {
-            HSCanvas()
-        }
-    }
-}
 
